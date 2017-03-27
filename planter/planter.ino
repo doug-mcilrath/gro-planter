583d3a92ddb00b3d37e30a5f709a83701b23864c
Leslie Chase <chasel2@rpi.edu>
2017-03-27 12:39:23 -0400

    Code
    Some code

---
+++ planter/planter.ino
@@ -0,0 +1,11 @@
+#include <RedBot.h>
+
+// Initialize Sensors
+RedBotSensor Sensor1 = RedBotSensor(A1);
+RedBotSensor Sensor2 = RedBotSensor(A2);
+RedBotSensor Sensor3 = RedBotSensor(A3);
+
+void loop() {
+  
+}
+
