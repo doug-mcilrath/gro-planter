ec7f8ec233f9a88364393ce438b73299dc7f4d02
Leslie Chase <chasel2@rpi.edu>
2017-03-31 12:44:12 -0400

    Code for light/sunlight integration - phase 1
    Code updated for initial testing on arduino. First will be tested with
    LED. Currently has boolean value for time set to on until integration
    with Doug.

--- planter/planter.ino
+++ planter/planter.ino
@@ -5,7 +5,23 @@ RedBotSensor Sensor1 = RedBotSensor(A1);
 RedBotSensor Sensor2 = RedBotSensor(A2);
 RedBotSensor Sensor3 = RedBotSensor(A3);
 
+int a;
+int on_bool = 0;
+
+#define LIGHT (7) // Light Logic Pin
+
+void setup(){
+ pinMode(LIGHT, OUTPUT);  // Set Light Pin to OUT
+}
+
 void loop() {
-  
+  on_bool = 1;
+  a = Sensor1.read();            // Calculate average sunlight from sensors
+  a += Sensor2.read();
+  a += Sensor3.read();
+  a /= 3;
+  if(a < 200 && (on_bool == 1)){            // Check to see if dark enough to turn on light, and if time for sunlight
+    digitalWrite(LIGHT, HIGH);
+  }
 }
 
