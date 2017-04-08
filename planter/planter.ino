d587069ceb1a14cb832f93a8089e8847fc2adea7
Leslie Chase <chasel2@rpi.edu>
2017-04-08 16:25:53 -0400

    Changed to use light resistor
    Updated with light resistor and new light- works

--- planter/planter.ino
+++ planter/planter.ino
@@ -1,26 +1,20 @@
-#include <RedBot.h>
 
-// Initialize Sensors
-RedBotSensor Sensor1 = RedBotSensor(A1);
-RedBotSensor Sensor2 = RedBotSensor(A2);
-//RedBotSensor Sensor3 = RedBotSensor(A3);
 
 int a;
 int on_bool = 0;
 
 #define LIGHT (7) // Light Logic Pin
+#define SENSOR (A0)
 
 void setup(){
  pinMode(LIGHT, OUTPUT);  // Set Light Pin to OUT
+ //pinMode(SENSOR, INPUT);
 }
 
 void loop() {
   on_bool = 1;
-  a = Sensor1.read();            // Calculate average sunlight from sensors
-  a += Sensor2.read();
-  //a += Sensor3.read();
-  a /= 2;
-  if(a < 200 && (on_bool == 1)){            // Check to see if dark enough to turn on light, and if time for sunlight
+  a = analogRead(SENSOR);
+  if(a < 900 && (on_bool == 1)){            // Check to see if dark enough to turn on light, and if time for sunlight
     digitalWrite(LIGHT, HIGH);
   }
   else{
